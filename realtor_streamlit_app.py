2f416df0b437eb02a63b8bd22d3f431c92a45f68
dmf95 <domenicfayad@gmail.com>
2021-04-04 16:09:23 -0700

    Added a png, and a dummy file that will be necessary to read into the app

--- realtor_streamlit_app.py
+++ realtor_streamlit_app.py
@@ -1,104 +1,224 @@
+# This app is for educational purpose only. Insights gained are not financial advice. Use at your own risk!
 import streamlit as st
-from streamlit_metrics import metric, metric_row
+from PIL import Image
 import pandas as pd
-from google.cloud import bigquery
-from google.cloud import bigquery_storage
-import google.auth
-import os
+import base64 as b
+import matplotlib.pyplot as plt
+from bs4 import BeautifulSoup
+import requests
+import json
 
-st.title('Vancouver Listing Prices')
-st.write('A descriptive analysis based on data retrieved on November 12, 2020 from REW')
+#------------------------------------#
+# 1) APP TITLE, DESCRIPTION & LAYOUT
+## Page expands to full width
+## Layout... Divide page to 3 columns (col1 = sidebar, col2 and col3 = page contents)
+st.set_page_config(layout="wide")
+#------------------------------------#
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="C:/Users/S Lo/Workspace/VancouverHousingPrices/Housing Project-db68639e1bff.json"
+# Title
+#------------------------------------#
 
-'''
-## Step 1: Import Data
-'''
+image = Image.open('real-estate.png')
 
-# Documentation for importing data from bigquery: https://cloud.google.com/bigquery/docs/bigquery-storage-python-pandas
-# Explicitly create a credentials object. This allows you to use the same
-# credentials for both the BigQuery and BigQuery Storage clients, avoiding
-# unnecessary API calls to fetch duplicate authentication tokens.
-credentials, your_project_id = google.auth.default(
-    scopes=["https://www.googleapis.com/auth/cloud-platform"]
-)
+st.image(image, width = 100)
 
-# Make clients.
-bqclient = bigquery.Client(credentials=credentials, project=your_project_id,)
-bqstorageclient = bigquery_storage.BigQueryReadClient(credentials=credentials)
+st.title('Real Estate Listing Prices')
+st.markdown("""
+This app helps visualize real estate listings data. Use the Sample dataset, or upload your own!
 
-# Construct a BigQuery client object.
-bqclient = bigquery.Client()
-
-query_string = """
-    SELECT *
-    FROM `housing-project-300900.ds_main.f_real_estate_listings_view`
-"""
-query_job = bqclient.query(query_string)  # Make an API request.
-
-df_list_price = (
-    bqclient.query(query_string)
-    .result()
-    .to_dataframe(bqstorage_client=bqstorageclient)
-)
-
-# Change data types
-df_list_price['scrape_dt'] = pd.to_datetime(df_list_price['scrape_dt'])
-df_list_price['bed'] = pd.to_numeric(df_list_price['bed'])
-df_list_price['bath'] = pd.to_numeric(df_list_price['bath'])
-df_list_price['price'] = pd.to_numeric(df_list_price['price'])
-df_list_price['strata_fee'] = pd.to_numeric(df_list_price['strata_fee'])
-
-# Inspect the raw data
-st.subheader('Listing prices data')
-st.write(df_list_price)
-
-'''
-## Step 2: Create KPI cards
-'''
-# Calculate KPIs
-listings_count = len(pd.unique(df_list_price['listing_id']))
-# TODO: calculate average days in market
-avg_market_days = 'placeholder'
+""")
 
 
-# Create visuals
-st.subheader('Vancouver Housing Inventory')
-metric_row(
-    {
-        "Number of listings": listings_count,
-        "Average days on market": avg_market_days
-    }
-)
+# About
+#------------------------------------#
 
-'''
-## Step 3: Create graphs
-'''
-# Create dataframe with count of unique listings by date
-df_inventory = df_list_price[['scrape_dt', 'listing_id']].groupby(['scrape_dt']).agg(['nunique']).reset_index()
-df_inventory.columns = ['scrape_dt', 'listings_count']
+expander_bar = st.beta_expander("About")
+expander_bar.markdown("""
+* **Python libraries:** base64, pandas, streamlit, numpy, matplotlib, seaborn, BeautifulSoup, requests, json, time
+* **Sample data source:** [CoinMarketCap](http://rew.ca).
+* **Credit:** Layout adapted from various example applications *[Build 12 Data Science Apps with Python and Streamlit - Full Course](https://www.youtube.com/watch?v=JwSS70SZdyM&ab_channel=freeCodeCamp.org)*  by [Chanin Nantasenamat](https://github.com/dataprofessor).
+""")
 
-st.subheader('Number of listings over time')
-st.line_chart(df_inventory.set_index('scrape_dt'))
 
-'''
-## Step 4: Create tables
-'''
-# Create dataframe with count of listings by property type and number of bedrooms
-df_prop_bed = df_list_price[['listing_id', 'property_type', 'bed']].groupby(['property_type', 'bed']).agg(['nunique']).reset_index()
-df_prop_bed.columns = ['property_type', 'bed', 'listings_count']
-df_prop_bed['bed'] = df_prop_bed['bed'].astype(str) + ' Bedroom(s)'
+# Layout
+#------------------------------------#
 
-# Create separate dataframes for each property type
-df_apt_bed = df_prop_bed[df_prop_bed['property_type'] == 'Apt/Condo']
-df_duplex_bed = df_prop_bed[df_prop_bed['property_type'] == 'Duplex']
-df_house_bed = df_prop_bed[df_prop_bed['property_type'] == 'House']
-df_rec_bed = df_prop_bed[df_prop_bed['property_type'] == 'Recreational']
-df_town_bed = df_prop_bed[df_prop_bed['property_type'] == 'Townhouse']
+col1 = st.sidebar
+col2, col3 = st.beta_columns((2,1)) # col1 is 2x greater than col2
 
-# TODO: create separate tabs for the 5 tables, potentially using Bokeh (https://discuss.streamlit.io/t/bokeh-can-provide-layouts-tabs-advanced-tables-and-js-callbacks-in-streamlit/1108)
-st.write(df_apt_bed)
-st.write(df_duplex_bed)
-st.write(df_house_bed)
-st.write(df_rec_bed)
-st.write(df_town_bed)
+#-----------------------------------#
+# 2) SIDEBAR
+## User upload data feature
+## Relevant & simple filtering options
+#-----------------------------------#
+
+## Sidebar title
+st.sidebar.header('User Input Features')
+
+## Example CSV input file download
+## Note for user: will need to right click "save link as", then save as ".csv"
+st.sidebar.markdown("""
+[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/penguins_example.csv)
+""")
+
+# Collects user input features into dataframe
+uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+
+# scenario 1: read in csv if it exists
+if uploaded_file is not None:
+    input_df = pd.read_csv(uploaded_file)
+
+# scenario 2: read in default values from input selection in the app
+else:
+    def user_input_features():
+        island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
+        sex = st.sidebar.selectbox('Sex',('male','female'))
+        bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
+        bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
+        flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
+        body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0,6300.0,4207.0)
+        data = {'island': island,
+                'bill_length_mm': bill_length_mm,
+                'bill_depth_mm': bill_depth_mm,
+                'flipper_length_mm': flipper_length_mm,
+                'body_mass_g': body_mass_g,
+                'sex': sex}
+        features = pd.DataFrame(data, index=[0])
+        return features
+    input_df = user_input_features()
+
+# Combines user input features with entire penguins dataset
+# This will be useful for the encoding phase
+penguins_raw = pd.read_csv('penguins_cleaned.csv')
+penguins = penguins_raw.drop(columns=['species']) # drop b/c its our target var
+df = pd.concat([input_df,penguins],axis=0)
+
+
+
+
+
+
+
+
+
+# TODO: MAIN PANEL SHIT
+
+
+
+
+# Web scraping of CoinMarketCap data
+@st.cache # only scrape once, then cache results
+def load_data():
+    cmc = requests.get('https://coinmarketcap.com')
+    soup = BeautifulSoup(cmc.content, 'html.parser')
+
+    data = soup.find('script', id='__NEXT_DATA__', type='application/json')
+    coins = {}
+    coin_data = json.loads(data.contents[0])
+    listings = coin_data['props']['initialState']['cryptocurrency']['listingLatest']['data']
+    for i in listings:
+      coins[str(i['id'])] = i['slug']
+
+    coin_name = []
+    coin_symbol = []
+    market_cap = []
+    percent_change_1h = []
+    percent_change_24h = []
+    percent_change_7d = []
+    price = []
+    volume_24h = []
+
+    for i in listings:
+      coin_name.append(i['slug'])
+      coin_symbol.append(i['symbol'])
+      price.append(i['quote'][currency_price_unit]['price'])
+      percent_change_1h.append(i['quote'][currency_price_unit]['percentChange1h'])
+      percent_change_24h.append(i['quote'][currency_price_unit]['percentChange24h'])
+      percent_change_7d.append(i['quote'][currency_price_unit]['percentChange7d'])
+      market_cap.append(i['quote'][currency_price_unit]['marketCap'])
+      volume_24h.append(i['quote'][currency_price_unit]['volume24h'])
+
+    df = pd.DataFrame(columns=['coin_name', 'coin_symbol', 'market_cap', 'percentChange1h', 'percentChange24h', 'percentChange7d', 'price', 'volume24h'])
+    df['coin_name'] = coin_name
+    df['coin_symbol'] = coin_symbol
+    df['price'] = price
+    df['percentChange1h'] = percent_change_1h
+    df['percentChange24h'] = percent_change_24h
+    df['percentChange7d'] = percent_change_7d
+    df['marketCap'] = market_cap
+    df['volume24h'] = volume_24h
+    return df
+
+df = load_data()
+
+## Sidebar - Cryptocurrency selections
+sorted_coin = sorted( df['coin_symbol'] )
+selected_coin = col1.multiselect('Cryptocurrency', sorted_coin, sorted_coin)
+
+df_selected_coin = df[ (df['coin_symbol'].isin(selected_coin)) ] # Filtering data
+
+## Sidebar - Number of coins to display
+num_coin = col1.slider('Display Top N Coins', 1, 100, 100)
+df_coins = df_selected_coin[:num_coin]
+
+## Sidebar - Percent change timeframe
+percent_timeframe = col1.selectbox('Percent change time frame',
+                                    ['7d','24h', '1h'])
+percent_dict = {"7d":'percent_change_7d',"24h":'percent_change_24h',"1h":'percent_change_1h'}
+selected_percent_timeframe = percent_dict[percent_timeframe]
+
+## Sidebar - Sorting values
+sort_values = col1.selectbox('Sort values?', ['Yes', 'No'])
+
+col2.subheader('Price Data of Selected Cryptocurrency')
+col2.write('Data Dimension: ' + str(df_selected_coin.shape[0]) + ' rows and ' + str(df_selected_coin.shape[1]) + ' columns.')
+
+col2.dataframe(df_coins)
+
+# Download CSV data
+# https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
+def filedownload(df):
+    csv = df.to_csv(index=False)
+    b64 = b.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
+    href = f'<a href="data:file/csv;base64,{b64}" download="crypto.csv">Download CSV File</a>'
+    return href
+
+col2.markdown(filedownload(df_selected_coin), unsafe_allow_html=True)
+
+#---------------------------------#
+# Preparing data for Bar plot of % Price change
+col2.subheader('Table of % Price Change')
+df_change = pd.concat([df_coins.coin_symbol, df_coins.percent_change_1h, df_coins.percent_change_24h, df_coins.percent_change_7d], axis=1)
+df_change = df_change.set_index('coin_symbol')
+df_change['positive_percent_change_1h'] = df_change['percent_change_1h'] > 0
+df_change['positive_percent_change_24h'] = df_change['percent_change_24h'] > 0
+df_change['positive_percent_change_7d'] = df_change['percent_change_7d'] > 0
+col2.dataframe(df_change)
+
+# Conditional creation of Bar plot (time frame)
+col3.subheader('Bar plot of % Price Change')
+
+if percent_timeframe == '7d':
+    if sort_values == 'Yes':
+        df_change = df_change.sort_values(by=['percent_change_7d'])
+    col3.write('*7 days period*')
+    plt.figure(figsize=(5,25))
+    plt.subplots_adjust(top = 1, bottom = 0)
+    df_change['percent_change_7d'].plot(kind='barh', color=df_change.positive_percent_change_7d.map({True: 'g', False: 'r'}))
+    col3.pyplot(plt)
+elif percent_timeframe == '24h':
+    if sort_values == 'Yes':
+        df_change = df_change.sort_values(by=['percent_change_24h'])
+    col3.write('*24 hour period*')
+    plt.figure(figsize=(5,25))
+    plt.subplots_adjust(top = 1, bottom = 0)
+    df_change['percent_change_24h'].plot(kind='barh', color=df_change.positive_percent_change_24h.map({True: 'g', False: 'r'}))
+    col3.pyplot(plt)
+else:
+    if sort_values == 'Yes':
+        df_change = df_change.sort_values(by=['percent_change_1h'])
+    col3.write('*1 hour period*')
+    plt.figure(figsize=(5,25))
+    plt.subplots_adjust(top = 1, bottom = 0)
+    df_change['percent_change_1h'].plot(kind='barh', color=df_change.positive_percent_change_1h.map({True: 'g', False: 'r'}))
+    col3.pyplot(plt)
